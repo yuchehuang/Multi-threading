a93444022738caff294a71f082726b9121f0b57e
Yuche Huang <53272971+yuchehuang@users.noreply.github.com>
2019-10-13 01:52:32 +0800

    Update Source.cpp

--- Code/thread/Source.cpp
+++ Code/thread/Source.cpp
@@ -6,13 +6,14 @@
 // Disable the warning 
 #pragma warning( disable : 4996 )
 int *dynamic_array_1, *dynamic_array_2,*result,*TLB, size, thread_num;
+const unsigned int Table_length=3;
 
 //-----Thread Function-----------------//
 int start_opint_calculate(int threadID)
 {
 	int start_opint = 0;
-	for (int id = 0; id < TLB[3 * threadID]; id++)			// TLB[2*id+1]= The number of elements this thread will execute
-		start_opint = start_opint + TLB[3 * id + 1];		//start_opint= before this thread how many element has been execute ------> thread_3's start point is  thread_1's number of element  + thread_2's number of element	
+	for (int id = 0; id < TLB[Table_length * threadID]; id++)			// TLB[2*id+1]= The number of elements this thread will execute
+		start_opint = start_opint + TLB[Table_length * id + 1];		//start_opint= before this thread how many element has been execute ------> thread_Table_length's start point is  thread_1's number of element  + thread_2's number of element	
 	
 	//printf("Thread %d start point is: %d\n ", threadID, start_opint);
 	return start_opint;
@@ -32,9 +33,9 @@ DWORD WINAPI Thread_dynamic(LPVOID lpParam)
 {
 	int id = (int)lpParam,temp;
 			
-	//printf("Thread %3d start point is %d  execute to %d\n",id, TLB[3 * id + 2], TLB[3 * id + 2] +TLB[3 * id+1]-1); //Test Each thread can get their own variable
+	//printf("Thread %3d start point is %d  execute to %d\n",id, TLB[Table_length * id + 2], TLB[Table_length * id + 2] +TLB[Table_length * id+1]-1); //Test Each thread can get their own variable
 	
-	for (int element = TLB[3 * id + 2]; element < TLB[3 * id + 2] + TLB[3 * id+1]; element++)
+	for (int element = TLB[Table_length * id + 2]; element < TLB[Table_length * id + 2] + TLB[Table_length * id+1]; element++)
 	{	
 		temp= execute_element_number(element);
 		result[element] = temp;
@@ -46,7 +47,7 @@ DWORD WINAPI Thread_dynamic(LPVOID lpParam)
 void number_of_each_thread_calculated_Table()
 {	
 	for (int i = 0; i < thread_num; i++)
-		printf("Thread %2d start from: %2d to %2d of elements  Execute %4d elements\n", i, TLB[3 * i + 2], TLB[3 * i + 1]+ TLB[3 * i + 2]-1, TLB[3 * i + 1]);
+		printf("Thread %2d start from: %2d to %2d of elements  Execute %4d elements\n", i, TLB[Table_length * i + 2], TLB[Table_length * i + 1]+ TLB[Table_length * i + 2]-1, TLB[Table_length * i + 1]);
 }
 bool Check_error(HANDLE thread1,  DWORD thread_id,int id)
 {
@@ -88,7 +89,7 @@ void thread_input(void)
 {
 	printf("Enter the number of thread: ");
 	scanf("%d", &thread_num);
-	TLB = new int[3 * thread_num];		// calculate  number of element  for per thread
+	TLB = new int[Table_length * thread_num];		// calculate  number of element  for per thread
 	//---------------Assign the number of element per thread will execute------------------------------//
 /*			      TLB:   ID		Time	start point
 				Thread	 0		 10			0
@@ -103,14 +104,14 @@ void assign()
 	remain = (size*size) % thread_num;
 	for (int i = 0; i < thread_num; i++)
 	{
-		TLB[3*i] = i;
-		TLB[3*i+1] = (size*size) / thread_num;
+		TLB[Table_length*i] = i;
+		TLB[Table_length*i+1] = (size*size) / thread_num;
 	}	
 	for (int i = remain; i > 0; i--)
-		TLB[3*(remain - i)+ 1] = TLB[3*(remain - i)+ 1] + 1;
+		TLB[Table_length*(remain - i)+ 1] = TLB[Table_length*(remain - i)+ 1] + 1;
 
 	for (int i = 0; i < thread_num; i++)
-		TLB[3 * i + 2] = start_opint_calculate(i);
+		TLB[Table_length * i + 2] = start_opint_calculate(i);
 }
 void print_input()
 {
@@ -133,7 +134,7 @@ int main(void)
 	DWORD  *thread_id = new DWORD[thread_num];
 	for (int i = 0; i < thread_num; i++)
 	{
-		thread[i] = CreateThread(NULL, 0, Thread_dynamic, (void*)TLB[3 * i], 0x00000004, &thread_id[i]); //(void*) to make variable compactable to the function	
+		thread[i] = CreateThread(NULL, 0, Thread_dynamic, (void*)TLB[Table_length * i], 0x00000004, &thread_id[i]); //(void*) to make variable compactable to the function	
 		
 		//if one of thread occurs error change the total number of thread
 		if (Check_error(thread[i], thread_id[i], i))
@@ -178,4 +179,3 @@ int main(void)
 	system("pause");
 	return 0;
 }
-
